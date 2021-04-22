50a5498e4ac4e74c2aada1b2e1a948090ee5ff0f
Serena Berry <serenaberry@nypl.org>
2021-04-22 14:16:39 -0400

    Changes to theme

--- src/ui/theme/components/button.ts
+++ src/ui/theme/components/button.ts
@@ -1,21 +1,77 @@
 const ButtonStyle = {
   // style object for base or default style
   baseStyle: {
-    borderRadius: 0,
+    borderRadius: 'none',
   },
   // styles for different sizes ("sm", "md", "lg")
   sizes: {},
   // styles for different visual variants ("outline", "solid")
   variants: {
-    outline: {
-      borderRadius: 0,
-    },
+    solid: variantSolid,
   },
-  // default values for `size` and `variant`
+  // default values for `size`, `variant`, `colorScheme`
   defaultProps: {
-    size: '',
-    variant: '',
+    size: 'md',
+    variant: 'solid',
+    colorScheme: 'light',
   },
 };
+
+/* Color Schemes:
+ ** Light, Sepia, Dark
+ * States:
+ ** Normal
+ ** Selected
+ ** Disabled
+ ** Hovered
+ */
+function variantSolid(props: any) {
+  const { colorScheme, isSelected } = props;
+
+  switch (colorScheme) {
+    // case 'sepia':
+    //   return {
+    //     bg: background,
+    //     color: mode(color, `gray.800`)(props),
+    //     _hover: {
+    //       bg: mode(hoverBg, `${colorScheme}.300`)(props),
+    //       _disabled: {
+    //         bg: background,
+    //       },
+    //     },
+    //     _active: { bg: mode(activeBg, `${colorScheme}.400`)(props) },
+    //   }
+    // case 'dark':
+    //   return {
+    //     bg: background,
+    //     color: mode(color, `gray.800`)(props),
+    //     _hover: {
+    //       bg: mode(hoverBg, `${colorScheme}.300`)(props),
+    //       _disabled: {
+    //         bg: background,
+    //       },
+    //     },
+    //     _active: { bg: mode(activeBg, `${colorScheme}.400`)(props) },
+    //   }
+    default:
+      // default is 'light'
+      return {
+        bg: isSelected ? 'green.500' : '#fff',
+        color: isSelected ? 'white' : `gray.500`,
+        _hover: {
+          bg: isSelected ? 'green.500' : `gray.200`,
+          // _disabled: {
+          //   bg: `gray.100`,
+          // },
+        },
+        _active: {
+          bg: `gray.300`,
+        },
+        _disabled: {
+          bg: `gray.100`,
+        },
+      };
+  }
+}
 
 export default ButtonStyle;
