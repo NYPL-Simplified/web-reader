4726b0eafbe9637c8bdb9bdf4659af52b1bb7262
kristojorg <kristo.jorgenson@gmail.com>
2021-04-23 08:36:41 -0400

    test fix

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { Default as Button } from '../stories/Button.stories';
 
 describe('Thing', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Button />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
