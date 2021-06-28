3b416655337342f9750888891fde296c9559804e
kristojorg <kristo.jorgenson@gmail.com>
2021-06-28 16:21:56 +0200

    disable snapshotting

--- tests/storyshots.test.ts
+++ tests/storyshots.test.ts
@@ -1,4 +1,4 @@
-// storybook.test.js
+// we are disabling this for now
 
-import initStoryshots from '@storybook/addon-storyshots';
-initStoryshots();
+// import initStoryshots from '@storybook/addon-storyshots';
+// initStoryshots();
