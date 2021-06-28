5f0afa852f51421815741891cc08afb517e989e7
kristojorg <kristo.jorgenson@gmail.com>
2021-06-28 20:26:23 +0200

    the final storybook fix ?

--- src/ServiceWorker/client.ts
+++
@@ -1,9 +0,0 @@
-import { Workbox } from 'workbox-window';
-
-/**
- * This file is to communicate with our service worker:
- *  - Telling it books to download
- *    - Whether the book needs to be decrypted
- *  - Querying which books are download
- *  - Receiving information on download progress
- */
