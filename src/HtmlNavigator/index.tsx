27d6f1530aa60b9f0161b2e36b7b1799a8d12e0a
kristojorg <kristo.jorgenson@gmail.com>
2021-07-07 19:35:08 +0200

    remove unused injectables

--- src/HtmlNavigator/index.tsx
+++ src/HtmlNavigator/index.tsx
@@ -29,34 +29,9 @@ export default class HtmlNavigator extends Navigator {
     const url = new URL(webpubManifestUrl);
     const reader = await D2Reader.build({
       url,
-      injectables: injectables as any,
-
-      // all of these were required
-      userSettings: {
-        verticalScroll: 'readium-scroll-off',
-      },
-      initialAnnotations: undefined,
-      lastReadingPosition: undefined,
-      upLinkUrl: undefined,
-      material: {
-        settings: {
-          fontOverride: false,
-          advancedSettings: false,
-          pageMargins: false,
-          lineHeight: false,
-        },
-      },
-      rights: {
-        autoGeneratePositions: false,
-      },
-      tts: undefined,
-      search: { color: 'red', current: 'blah' },
-      annotations: { initialAnnotationColor: 'blue' },
-      highlighter: { selectionMenuItems: [] },
-      useLocalStorage: false,
-      attributes: { margin: 2 },
-      // TODO: Fix this any assertion
-    } as any);
+      injectables: injectables,
+      injectablesFixed: [],
+    });
 
     const navigator = new HtmlNavigator(didMutate, reader);
     return navigator;
@@ -169,46 +144,21 @@ const injectables = [
     url: 'http://localhost:1234/viewer/readium-css/ReadiumCSS-after.css',
     r2after: true,
   },
-  {
-    type: 'style',
-    url: 'http://localhost:1234/viewer/injectables/pagebreak/pagebreak.css',
-    r2after: true,
-  },
-  // { type: 'style', url: 'http://localhost:1234/viewer/readium-css/neon-after.css', r2after: true, appearance: 'neon' },
-  {
-    type: 'script',
-    url:
-      'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.6/MathJax.js?config=TeX-MML-AM_CHTML&latest',
-  },
-  {
-    type: 'style',
-    url: 'http://localhost:1234/viewer/fonts/opendyslexic/opendyslexic.css',
-    fontFamily: 'opendyslexic',
-    systemFont: false,
-  },
-  { type: 'style', fontFamily: 'Courier', systemFont: true },
   // {
-  //   type: 'script',
-  //   url: 'http://localhost:1234/viewer/injectables/click/click.js',
+  //   type: 'style',
+  //   url: 'http://localhost:1234/viewer/injectables/pagebreak/pagebreak.css',
+  //   r2after: true,
   // },
   // {
   //   type: 'script',
-  //   url: 'http://localhost:1234/viewer/injectables/footnotes/footnotes.js',
+  //   url:
+  //     'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.6/MathJax.js?config=TeX-MML-AM_CHTML&latest',
   // },
-  {
-    type: 'style',
-    url: 'http://localhost:1234/viewer/injectables/footnotes/footnotes.css',
-  },
   // {
-  //   type: 'script',
-  //   url: 'http://localhost:1234/viewer/injectables/glossary/glossary.js',
+  //   type: 'style',
+  //   url: 'http://localhost:1234/viewer/fonts/opendyslexic/opendyslexic.css',
+  //   fontFamily: 'opendyslexic',
+  //   systemFont: false,
   // },
-  {
-    type: 'style',
-    url: 'http://localhost:1234/viewer/injectables/glossary/glossary.css',
-  },
-  {
-    type: 'style',
-    url: 'http://localhost:1234/viewer/injectables/style/style.css',
-  },
+  // { type: 'style', fontFamily: 'Courier', systemFont: true },
 ];
