1a4f5cdd2a85a35db57b307db0afc8bf80586e3f
Leon <toxiapo@gmail.com>
2021-07-07 13:07:50 -0400

    add tests to Button and ToggleGroup component (#12)
    * redoing test changes after git whoopsie
    
    * fix jest
    
    * rename jest file back to ts
    
    Co-authored-by: kristojorg <kristo.jorgenson@gmail.com>
    Co-authored-by: Kristo Jorgenson <kristojorg@users.noreply.github.com>

---
+++ tests/ToggleGroup.test.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import ToggleButton from '../src/ui/ToggleButton';
+import ToggleGroup from '../src/ui/ToggleGroup';
+
+const renderComponent = () => {
+  return render(
+    <ToggleGroup value="sedona">
+      <ToggleButton value="sedona">Sedona</ToggleButton>
+      <ToggleButton value="santa_fe">Santa Fe</ToggleButton>
+      <ToggleButton value="las_cruces">Las Cruces</ToggleButton>
+    </ToggleGroup>
+  );
+};
+
+test('radiogroup should be in the document', () => {
+  const { getByRole } = renderComponent();
+  expect(getByRole('radiogroup')).toBeInTheDocument();
+});
+
+test('toggle group should contain all radio buttons', () => {
+  const { queryAllByRole } = renderComponent();
+  expect(queryAllByRole('radio')).toHaveLength(3);
+});
+
+test('respect value props', () => {
+  const { getByLabelText } = renderComponent();
+
+  expect(getByLabelText('Sedona')).toBeChecked();
+
+  fireEvent.click(getByLabelText('Santa Fe'));
+
+  expect(getByLabelText('Sedona')).toBeChecked();
+});
+
+test('onChange callback function should be called', () => {
+  const onChangeHandler = jest.fn();
+  const { getByLabelText } = render(
+    <ToggleGroup value="sedona" onChange={onChangeHandler}>
+      <ToggleButton value="sedona">Sedona</ToggleButton>
+      <ToggleButton value="santa_fe">Santa Fe</ToggleButton>
+      <ToggleButton value="las_cruces">Las Cruces</ToggleButton>
+    </ToggleGroup>
+  );
+
+  fireEvent.click(getByLabelText('Santa Fe'));
+
+  expect(onChangeHandler).toHaveBeenCalledWith('santa_fe');
+});
