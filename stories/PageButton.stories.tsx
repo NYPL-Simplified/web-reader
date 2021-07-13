6c12c71344c17a5612dcc12c65acab6c49109cba
toxiapo <toxiapo@gmail.com>
2021-07-13 12:48:26 -0400

    add PageButton Stories

---
+++ stories/PageButton.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import PageButton, { PageButtonProps } from '../src/ui/PageButton';
+
+const meta: Meta = {
+  title: 'PageButton',
+  component: PageButton,
+  argTypes: {
+    children: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+};
+
+export default meta;
+
+const Template: Story<PageButtonProps> = (args) => <PageButton {...args} />;
+
+// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
+// https://storybook.js.org/docs/react/workflows/unit-testing
+export const PreviousButton = Template.bind({});
+
+PreviousButton.args = {
+  children: '<',
+};
