a7959e501ccde09288201a389bc9a8957e4d4bbb
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2021-08-04 17:56:15 +0200

    fix react-pdf

---
+++ src/PdfReader/react-pdf.d.ts
@@ -0,0 +1,6 @@
+/**
+ * This is a declaration file to fix the types for the parcel entrypoint
+ */
+declare module 'react-pdf/dist/esm/entry.parcel' {
+  export { Document, Page } from 'react-pdf';
+}
