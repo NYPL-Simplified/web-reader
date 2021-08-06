7cbb37ce51841148da79cfe7b0427e98a7aed9a7
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2021-08-06 16:16:23 +0200

    adjust to use color mode in manager

--- src/ui/manager.tsx
+++ src/ui/manager.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider, Flex } from '@chakra-ui/react';
 import * as React from 'react';
 import { ReaderReturn } from '../types';
 import Header from './Header';
+import useColorModeValue from './hooks/useColorModeValue';
 import PageButton from './PageButton';
 import { getTheme } from './theme';
 
@@ -10,46 +11,40 @@ import { getTheme } from './theme';
  * that can be imported and used separately or in a customized setup.
  * It takes the return value of useWebReader as props
  */
-const ManagerUI: React.FC<ReaderReturn> = ({ children, navigator, state }) => {
+const ManagerUI: React.FC<ReaderReturn> = (props) => {
   return (
-    <ThemeProvider theme={getTheme(state?.colorMode ?? 'day')}>
-      <Flex flexDir="column" minHeight="100vh">
-        {navigator && state && (
-          <Header readerState={state} navigator={navigator} />
-        )}
-        <PageButton onClick={navigator?.goBackward} left={0}>{`<`}</PageButton>
-        <Flex
-          w="100vw"
-          // accounting for the prev/next buttons
-          px={{ sm: 10, md: 16 }}
-          flexDir="column"
-          alignItems="stretch"
-          flex="1 0 auto"
-        >
-          {children}
-        </Flex>
-        <PageButton onClick={navigator?.goForward} right={0}>{`>`}</PageButton>
-      </Flex>
+    <ThemeProvider theme={getTheme(props.state?.colorMode ?? 'day')}>
+      <WebReaderContent {...props} />
     </ThemeProvider>
   );
 };
 
-// const Option: React.FC<{ item: TocItem; level: number }> = ({
-//   item,
-//   level = 0,
-// }) => {
-//   const indents = '\u00A0'.repeat(level * 2);
-//   return (
-//     <>
-//       <option value={item.href} style={{ paddingLeft: 2 * level }}>
-//         {indents}
-//         {item.title}
-//       </option>
-//       {item.children?.map((child) => (
-//         <Option item={child} level={level + 1} />
-//       ))}
-//     </>
-//   );
-// };
+const WebReaderContent: React.FC<ReaderReturn> = ({
+  children,
+  navigator,
+  state,
+}) => {
+  const bgColor = useColorModeValue('ui.white', 'ui.black', 'ui.sepia');
+  return (
+    <Flex flexDir="column" minHeight="100vh" w="100vw">
+      {navigator && state && (
+        <Header readerState={state} navigator={navigator} />
+      )}
+      <PageButton onClick={navigator?.goBackward} left={0}>{`<`}</PageButton>
+      <Flex
+        width="100vw"
+        bg={bgColor}
+        // accounting for the prev/next buttons
+        px={{ sm: 10, md: '5vw' }}
+        flexDir="column"
+        alignItems="stretch"
+        flex="1 0 auto"
+      >
+        {children}
+      </Flex>
+      <PageButton onClick={navigator?.goForward} right={0}>{`>`}</PageButton>
+    </Flex>
+  );
+};
 
 export default ManagerUI;
