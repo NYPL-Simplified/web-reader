3abe73a05c39c639596bcf028cbc9d305cfaa886
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2021-08-09 11:36:42 +0200

    install SW in example app

--- example/serviceWorker.ts
+++ example/serviceWorker.ts
@@ -1,1 +1,8 @@
-import '../src/ServiceWorker';
+import initWebReaderSW from '../src/ServiceWorker';
+
+initWebReaderSW();
+
+/**
+ * So we need to cache things. We could just call cache.add, but then we can't really
+ * know if a publication is fully cached.
+ */
