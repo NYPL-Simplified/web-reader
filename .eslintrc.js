65d9126d5e81bc37d97eee32a975d5a1ec25e7d1
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2021-08-19 14:00:49 +0200

    update lint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,17 @@
 module.exports = {
-  "extends": [
-    "react-app",
-    "prettier",
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'react-app',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
   ],
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
-  }
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
