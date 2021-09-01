d9861959910d1d881c942afd5606c349105875cd
toxiapo <toxiapo@gmail.com>
2021-09-01 17:24:32 -0400

    update file structure

--- src/ui/SettingsButton.tsx
+++ src/ui/SettingsButton.tsx
@@ -4,173 +4,15 @@ import {
   PopoverTrigger,
   PopoverContent,
   PopoverBody,
-  ButtonGroup,
   Text,
-  Circle,
 } from '@chakra-ui/react';
-import {
-  Navigator,
-  PdfReaderState,
-  HtmlReaderState,
-  PDFActiveReader,
-  HTMLActiveReader,
-} from '../types';
+import { PDFActiveReader, HTMLActiveReader } from '../types';
 import { Icon, IconNames } from '@nypl/design-system-react-components';
 
 import Button from './Button';
-import ToggleButton from './ToggleButton';
-import ToggleGroup from './ToggleGroup';
 import useColorModeValue from './hooks/useColorModeValue';
-
-const PDFSettings = ({
-  navigator,
-  readerState,
-  paginationValue,
-}: {
-  navigator: Navigator;
-  readerState: PdfReaderState;
-  paginationValue: string;
-}): React.ReactElement => {
-  return (
-    <>
-      <ToggleGroup
-        value={readerState.fontFamily}
-        label="text font options"
-        onChange={navigator.setFontFamily}
-      >
-        <ToggleButton value="publisher">Publisher</ToggleButton>
-        <ToggleButton value="serif">Serif</ToggleButton>
-        <ToggleButton value="sans-serif">Sans-Serif</ToggleButton>
-        <ToggleButton value="open-dyslexic">Dyslexia-Friendly</ToggleButton>
-      </ToggleGroup>
-      <ButtonGroup d="flex" spacing={0}>
-        <Button
-          flexGrow={1}
-          aria-label="Zoom In"
-          onClick={navigator.decreaseFontSize}
-          variant="toggle"
-        >
-          Zoom
-          <Circle
-            border="1px solid"
-            p={1}
-            ml={1}
-            size="17px"
-            alignItems="baseline"
-          >
-            <Icon decorative name={IconNames.minus} modifiers={['small']} />{' '}
-          </Circle>
-        </Button>
-        <Button
-          flexGrow={1}
-          aria-label="Zoom Out"
-          onClick={navigator.increaseFontSize}
-          variant="toggle"
-        >
-          Zoom
-          <Circle
-            border="1px solid"
-            p={1}
-            ml={1}
-            size="17px"
-            alignItems="baseline"
-          >
-            <Icon decorative name={IconNames.plus} modifiers={['small']} />{' '}
-          </Circle>
-        </Button>
-      </ButtonGroup>
-      <ToggleGroup
-        onChange={navigator.setScroll}
-        value={paginationValue}
-        label="pagination options"
-      >
-        <ToggleButton value="paginated">Paginated</ToggleButton>
-        <ToggleButton value="scrolling">Scrolling</ToggleButton>
-      </ToggleGroup>
-    </>
-  );
-};
-
-const HTMLSettings = ({
-  navigator,
-  readerState,
-  paginationValue,
-}: {
-  navigator: Navigator;
-  readerState: HtmlReaderState;
-  paginationValue: string;
-}): React.ReactElement => {
-  return (
-    <>
-      <ToggleGroup
-        value={readerState.fontFamily}
-        label="text font options"
-        onChange={navigator.setFontFamily}
-      >
-        <ToggleButton value="publisher">Publisher</ToggleButton>
-        <ToggleButton value="serif">Serif</ToggleButton>
-        <ToggleButton value="sans-serif">Sans-Serif</ToggleButton>
-        <ToggleButton value="open-dyslexic">Dyslexia-Friendly</ToggleButton>
-      </ToggleGroup>
-      <ButtonGroup d="flex" spacing={0}>
-        <Button
-          flexGrow={1}
-          aria-label="Decrease font size"
-          onClick={navigator.decreaseFontSize}
-          variant="toggle"
-        >
-          A-
-        </Button>
-        <Button
-          flexGrow={1}
-          aria-label="Increase font size"
-          onClick={navigator.increaseFontSize}
-          variant="toggle"
-        >
-          A+
-        </Button>
-      </ButtonGroup>
-      <ToggleGroup
-        value={readerState.colorMode}
-        label="reading theme options"
-        onChange={navigator.setColorMode}
-      >
-        <ToggleButton
-          colorMode="day"
-          value="day"
-          _checked={{ bg: 'ui.white' }} // default _checked color is green for toggles
-        >
-          Day
-        </ToggleButton>
-        <ToggleButton
-          colorMode="sepia"
-          value="sepia"
-          bg="ui.sepia" // distinct case where default needs to be sepia
-          _active={{ bg: 'ui.sepia' }}
-          _hover={{ bg: 'ui.sepia' }}
-          _checked={{ bg: 'ui.sepia' }}
-        >
-          Sepia
-        </ToggleButton>
-        <ToggleButton
-          colorMode="night"
-          value="night"
-          _checked={{ bg: 'ui.black' }}
-        >
-          Night
-        </ToggleButton>
-      </ToggleGroup>
-      <ToggleGroup
-        onChange={navigator.setScroll}
-        value={paginationValue}
-        label="pagination options"
-      >
-        <ToggleButton value="paginated">Paginated</ToggleButton>
-        <ToggleButton value="scrolling">Scrolling</ToggleButton>
-      </ToggleGroup>
-    </>
-  );
-};
+import PdfSettings from './PdfSettings';
+import HtmlSettings from './HtmlSettings';
 
 type SettingsCardProps =
   | Pick<PDFActiveReader, 'navigator' | 'state' | 'type'>
@@ -183,7 +25,8 @@ export default function SettingsCard(
   const open = () => setIsOpen(!isOpen);
   const close = () => setIsOpen(false);
 
-  const paginationValue = props.state?.isScrolling ? 'scrolling' : 'paginated';
+  const { navigator, state, type } = props;
+  const paginationValue = state?.isScrolling ? 'scrolling' : 'paginated';
   const contentBgColor = useColorModeValue('ui.white', 'ui.black', 'ui.white');
 
   return (
@@ -206,17 +49,17 @@ export default function SettingsCard(
           bgColor={contentBgColor}
         >
           <PopoverBody p={0} maxWidth="95vw">
-            {props.type === 'PDF' && (
-              <PDFSettings
-                navigator={props.navigator}
-                readerState={props.state}
+            {type === 'PDF' && (
+              <PdfSettings
+                navigator={navigator}
+                readerState={state}
                 paginationValue={paginationValue}
               />
             )}
-            {props.type === 'HTML' && (
-              <HTMLSettings
-                navigator={props.navigator}
-                readerState={props.state}
+            {type === 'HTML' && (
+              <HtmlSettings
+                navigator={navigator}
+                readerState={state}
                 paginationValue={paginationValue}
               />
             )}
