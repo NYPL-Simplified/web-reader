e0db169ea5763c1b2bfba7e1464b2ea0f51bfbdc
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2021-10-03 00:41:56 +0200

    exports

--- src/index.tsx
+++ src/index.tsx
@@ -33,3 +33,9 @@ export default WebReader;
 
 export { usePublicationSW } from './ServiceWorker/index';
 export { default as useWebReader } from './useWebReader';
+export { default as useHtmlReader } from './HtmlReader';
+export { default as usePdfReader } from './PdfReader';
+export {
+  defaultInjectables,
+  defaultInjectablesFixed,
+} from './HtmlReader/injectables';
