4f09f959ad16da46c7c9d962a252ec0cbddc17da
Samantha Andrews <samantharaeandrews@gmail.com>
2021-10-06 09:17:37 -0600

    can it load the page

--- cypress/integration/html-reader/settings.ts
+++ cypress/integration/html-reader/settings.ts
@@ -52,26 +52,32 @@ describe('useHtmlReader configuration settings', () => {
   it('should have no injectables by default', () => {
     cy.loadPage('/test/no-injectables');
 
-    cy.getIframeHtml().within(() => {
-      cy.get('head > link').should('not.exist');
-      // make sure there is a title, the query does in fact work
-      cy.get('head > title').should('exist');
-    });
+    // cy.getIframeHtml().within(() => {
+    //   cy.get('head > link').should('not.exist');
+    //   // make sure there is a title, the query does in fact work
+    //   cy.get('head > title').should('exist');
+    // });
 
-    cy.getIframeHead(IFRAME_SELECTOR).find('link').should('not.exist');
+    // cy.getIframeHtml().within(() => {
+    //   cy.get('head > link').should('not.exist');
+    //   // make sure there is a title, the query does in fact work
+    //   cy.get('head > title').should('exist');
+    // });
+
+    // cy.getIframeHead(IFRAME_SELECTOR).find('link').should('not.exist');
   });
 
   it('should render css injectables when provided', () => {
     cy.loadPage('/test/with-injectables');
 
-    cy.getIframeHtml().within(() => {
-      cy.get('head > title').should('exist');
-      cy.get(
-        'head > link[href$="/fonts/opensyslexic/opendyslexic.css"]'
-      ).should('exist');
-      cy.get('head > link[href$="/css/sample.css"]').should('exist');
-      // make sure the css was applied
-      cy.get('body').should('have.css', 'color', 'rgb(0, 0, 255)');
-    });
+    // cy.getIframeHtml().within(() => {
+    //   cy.get('head > title').should('exist');
+    //   cy.get(
+    //     'head > link[href$="/fonts/opensyslexic/opendyslexic.css"]'
+    //   ).should('exist');
+    //   cy.get('head > link[href$="/css/sample.css"]').should('exist');
+    //   // make sure the css was applied
+    //   cy.get('body').should('have.css', 'color', 'rgb(0, 0, 255)');
+    // });
   });
 });
