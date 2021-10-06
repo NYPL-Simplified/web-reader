9ca80388a264507f941ea334e7e82b26bf5ee334
Samantha Andrews <samantharaeandrews@gmail.com>
2021-10-06 11:13:30 -0600

    trying visit instead of custom command

--- cypress/integration/html-reader/settings.ts
+++ cypress/integration/html-reader/settings.ts
@@ -50,30 +50,31 @@ describe('display settings', () => {
 
 describe('useHtmlReader configuration settings', () => {
   it('should have no injectables by default', () => {
-    cy.loadPage('/test/no-injectables');
-    // cy.getIframeHead(IFRAME_SELECTOR).find('link').should('not.exist');
+    cy.visit('/test/no-injectables');
+    cy.wait(3000);
+    cy.getIframeHead(IFRAME_SELECTOR).find('link').should('not.exist');
     // make sure there is a title, the query does in fact work
     cy.getIframeHead(IFRAME_SELECTOR).contains('title', 'Cover');
   });
 
-  // it('should render css injectables when provided', () => {
-  //   cy.loadPage('/test/with-injectables');
-  //   cy.wait(3000);
-  //   cy.getIframeHead(IFRAME_SELECTOR).contains('title', 'Cover');
-  //   cy.getIframeHead(IFRAME_SELECTOR)
-  //     .find(
-  //       `link[href="${
-  //         Cypress.config().baseUrl
-  //       }/fonts/opensyslexic/opendyslexic.css"]`
-  //     )
-  //     .should('exist');
-  //   cy.getIframeHead(IFRAME_SELECTOR)
-  //     .find(`link[href="${Cypress.config().baseUrl}/css/sample.css"]`)
-  //     .should('exist');
-  //   cy.getIframeBody(IFRAME_SELECTOR).should(
-  //     'have.css',
-  //     'color',
-  //     'rgb(0, 0, 255)'
-  //   );
-  // });
+  it('should render css injectables when provided', () => {
+    cy.visit('/test/with-injectables');
+    cy.wait(3000);
+    cy.getIframeHead(IFRAME_SELECTOR).contains('title', 'Cover');
+    cy.getIframeHead(IFRAME_SELECTOR)
+      .find(
+        `link[href="${
+          Cypress.config().baseUrl
+        }/fonts/opensyslexic/opendyslexic.css"]`
+      )
+      .should('exist');
+    cy.getIframeHead(IFRAME_SELECTOR)
+      .find(`link[href="${Cypress.config().baseUrl}/css/sample.css"]`)
+      .should('exist');
+    cy.getIframeBody(IFRAME_SELECTOR).should(
+      'have.css',
+      'color',
+      'rgb(0, 0, 255)'
+    );
+  });
 });
