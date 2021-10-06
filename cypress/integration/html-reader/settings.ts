d265649c65a5524f9803bdf551808ab7c25fe493
Samantha Andrews <samantharaeandrews@gmail.com>
2021-10-06 09:36:50 -0600

    trying contains instead of find

--- cypress/integration/html-reader/settings.ts
+++ cypress/integration/html-reader/settings.ts
@@ -51,21 +51,19 @@ describe('display settings', () => {
 describe('useHtmlReader configuration settings', () => {
   it('should have no injectables by default', () => {
     cy.loadPage('/test/no-injectables');
-    cy.getIframeHead(IFRAME_SELECTOR).find('link').should('not.exist');
+    cy.getIframeHead(IFRAME_SELECTOR).contains('link');
     // make sure there is a title, the query does in fact work
-    cy.getIframeHead(IFRAME_SELECTOR).find('title').should('exist');
+    cy.getIframeHead(IFRAME_SELECTOR).contains('title');
   });
 
   it('should render css injectables when provided', () => {
     cy.loadPage('/test/with-injectables');
 
-    cy.getIframeHead(IFRAME_SELECTOR).find('title').should('exist');
-    cy.getIframeHead(IFRAME_SELECTOR)
-      .find('link[href$="/fonts/opensyslexic/opendyslexic.css"]')
-      .should('exist');
-    cy.getIframeHead(IFRAME_SELECTOR)
-      .find('link[href$="/css/sample.css"]')
-      .should('exist');
+    cy.getIframeHead(IFRAME_SELECTOR).contains('title');
+    cy.getIframeHead(IFRAME_SELECTOR).contains(
+      'link[href$="/fonts/opensyslexic/opendyslexic.css"]'
+    );
+    cy.getIframeHead(IFRAME_SELECTOR).contains('link[href$="/css/sample.css"]');
     cy.getIframeBody(IFRAME_SELECTOR).should(
       'have.css',
       'color',
