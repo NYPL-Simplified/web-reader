bc5bf897de7d6cd7d6090cca37ba5b1c39dd0ac4
Samantha Andrews <samantharaeandrews@gmail.com>
2021-10-06 11:25:02 -0600

    trying iframe body only

--- cypress/integration/html-reader/settings.ts
+++ cypress/integration/html-reader/settings.ts
@@ -49,28 +49,28 @@ describe('display settings', () => {
 });
 
 describe('useHtmlReader configuration settings', () => {
-  it('should have no injectables by default', () => {
-    cy.visit('/test/no-injectables');
-    cy.wait(3000);
-    cy.getIframeHead(IFRAME_SELECTOR).find('link').should('not.exist');
-    // make sure there is a title, the query does in fact work
-    cy.getIframeHead(IFRAME_SELECTOR).contains('title', 'Cover');
-  });
+  // it('should have no injectables by default', () => {
+  //   cy.visit('/test/no-injectables');
+  //   cy.wait(3000);
+  //   cy.getIframeHead(IFRAME_SELECTOR).find('link').should('not.exist');
+  //   // make sure there is a title, the query does in fact work
+  //   cy.getIframeHead(IFRAME_SELECTOR).contains('title', 'Cover');
+  // });
 
   it('should render css injectables when provided', () => {
     cy.visit('/test/with-injectables');
     cy.wait(3000);
-    cy.getIframeHead(IFRAME_SELECTOR).contains('title', 'Cover');
-    cy.getIframeHead(IFRAME_SELECTOR)
-      .find(
-        `link[href="${
-          Cypress.config().baseUrl
-        }/fonts/opensyslexic/opendyslexic.css"]`
-      )
-      .should('exist');
-    cy.getIframeHead(IFRAME_SELECTOR)
-      .find(`link[href="${Cypress.config().baseUrl}/css/sample.css"]`)
-      .should('exist');
+    // cy.getIframeHead(IFRAME_SELECTOR).contains('title', 'Cover');
+    // cy.getIframeHead(IFRAME_SELECTOR)
+    //   .find(
+    //     `link[href="${
+    //       Cypress.config().baseUrl
+    //     }/fonts/opensyslexic/opendyslexic.css"]`
+    //   )
+    //   .should('exist');
+    // cy.getIframeHead(IFRAME_SELECTOR)
+    //   .find(`link[href="${Cypress.config().baseUrl}/css/sample.css"]`)
+    //   .should('exist');
     cy.getIframeBody(IFRAME_SELECTOR).should(
       'have.css',
       'color',
