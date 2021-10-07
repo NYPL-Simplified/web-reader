1304e12465c887ba4600fa50d6d61f28a7b7625c
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2021-10-07 16:12:35 +0200

    add test

--- cypress/integration/html-reader/errors.ts
+++ cypress/integration/html-reader/errors.ts
@@ -1,8 +1,26 @@
 describe('error states', () => {
+  beforeEach(() => {
+    /**
+     * React re-throws caught exceptions in dev so that they appear in the
+     * console, but this makes cypress think it was uncaught, so we have to
+     * turn this off
+     */
+    cy.on('uncaught:exception', (err, runnable) => {
+      // returning false here prevents Cypress from
+      // failing the test
+      return false;
+    });
+  });
+
   it('throws error for unparseable manifest', () => {
     cy.loadPage('/test/unparseable-manifest');
 
-    // cy.findByRole();
+    cy.findByRole('heading', { name: 'An error occurred' });
+    cy.findByRole('alert', {
+      name: `Network Error: Unparseable JSON file found at ${
+        Cypress.config().baseUrl
+      }/samples/test/unparseable-manifest.json.`,
+    });
   });
 
   // it('throws error for malformed manifest', () => {});
