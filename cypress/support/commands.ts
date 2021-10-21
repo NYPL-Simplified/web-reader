d4b3b1bd678be55b3fc8c1d83467e99dcf1cf3ad
Samantha Andrews <samantharaeandrews@gmail.com>
2021-10-21 16:05:10 -0600

    removing check for iframe in load page command

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -47,11 +47,7 @@ Cypress.Commands.add('loadPage', (pageName) => {
   cy.wait('@sample', { timeout: 20000 }).then((interception) => {
     assert.isNotNull(interception?.response?.body, 'API call has data');
   });
-  cy.findByRole('link', { name: 'Return to Homepage' }).should('exist');
   cy.get('#reader-loading').should('not.be.visible');
-  cy.get(IFRAME_SELECTOR)
-    .its(`0.contentDocument.documentElement`)
-    .should('not.be.empty');
 });
 
 Cypress.Commands.add('getIframeHtml', (selector: string = IFRAME_SELECTOR) => {
