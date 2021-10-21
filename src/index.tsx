754ef26ef3b12f70ee15c52f7f41a9f440e34a5f
Kristo Jorgenson <kristojorg@users.noreply.github.com>
2021-10-21 15:40:10 +0200

    Add getTheme export (#90)
    * Add getTheme export
    
    This allows for easy reuse of the theming system
    
    * add type def to getTheme
    
    Co-authored-by: Anton Melser <anton@melser.org>

--- src/index.tsx
+++ src/index.tsx
@@ -35,3 +35,4 @@ export { usePublicationSW } from './ServiceWorker/index';
 export { default as useWebReader } from './useWebReader';
 export { default as useHtmlReader } from './HtmlReader';
 export { default as usePdfReader } from './PdfReader';
+export { getTheme } from './ui/theme';
