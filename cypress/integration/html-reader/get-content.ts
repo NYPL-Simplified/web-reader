c796678143d6d64c417483cf11be43218127eebb
toxiapo <toxiapo@gmail.com>
2021-11-01 23:42:32 -0400

    load page with native api

--- cypress/integration/html-reader/get-content.ts
+++ cypress/integration/html-reader/get-content.ts
@@ -2,7 +2,7 @@ import { IFRAME_SELECTOR } from '../../support/constants';
 
 describe('getContent', () => {
   it('getContent returns HTML', () => {
-    cy.loadPage('/test/get-content');
+    cy.visit('/test/get-content');
     cy.getIframeBody(IFRAME_SELECTOR)
       .find('p')
       .should('include.text', '/samples/moby-epub2-exploded/OEBPS/');
