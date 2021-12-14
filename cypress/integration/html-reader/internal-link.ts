9c6602799967617379853518cb4a5764ba2c93fe
toxiapo <toxiapo@gmail.com>
2021-12-14 17:59:24 -0500

    more assertion

--- cypress/integration/html-reader/internal-link.ts
+++ cypress/integration/html-reader/internal-link.ts
@@ -7,7 +7,7 @@ describe('navigating an EPUB page using internal link', () => {
       return false;
     });
 
-    cy.visit('/moby-epub2', {
+    cy.visit('moby-epub2', {
       onBeforeLoad: (win) => {
         win.sessionStorage.clear(); // clear storage so that we are always on page one
       },
@@ -17,11 +17,18 @@ describe('navigating an EPUB page using internal link', () => {
   it('navigates user using the internal TOC link on the page', () => {
     cy.log('Go to a page with TOC');
 
+    cy.getIframeBody(IFRAME_SELECTOR)
+      .find('img', { timeout: 5000 })
+      .should('have.attr', 'alt', 'Cover');
+
+    cy.findByRole('button', { name: 'Table of Contents' }).should('exist');
+
     cy.findByRole('button', { name: 'Table of Contents' }).click();
     cy.findByLabelText('MOBY-DICK; or, THE WHALE.').click();
 
     cy.wait(3000);
 
+    cy.findByRole('button', { name: 'Next Page' }).should('exist');
     cy.findByRole('button', { name: 'Next Page' }).click();
 
     cy.log('Go to ETYMOLOGY');
