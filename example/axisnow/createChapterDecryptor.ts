f93e8ce4c0bf37ccc7703fee4c580998f14d309a
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2022-04-04 12:09:35 +0200

    let example build without decryptor

--- example/axisnow/createChapterDecryptor.ts
+++ example/axisnow/createChapterDecryptor.ts
@@ -1,11 +1,13 @@
-import {
-  WebpubDecryptor,
-  WebpubDecryptorParams,
-} from '@nypl-simplified-packages/axisnow-access-control-web';
-
 export default async function createChapterDecryptor(
-  params: WebpubDecryptorParams
-): Promise<(url: string) => Promise<string>> {
-  const decryptor = await WebpubDecryptor.createDecryptor(params);
-  return decryptor.decryptResource;
+  params: unknown
+): Promise<null | ((url: string) => Promise<string>)> {
+  if (process.env.AXISNOW_VAULT_UUID && process.env.AXISNOW_ISBN) {
+    const {
+      WebpubDecryptor,
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+    } = require('@nypl-simplified-packages/axisnow-access-control-web');
+    const decryptor = await WebpubDecryptor.createDecryptor(params);
+    return decryptor.decryptResource;
+  }
+  return null;
 }
