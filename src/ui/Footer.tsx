6323c61d96fe9379da2f31118ae0a1279b55172e
toxiapo <toxiapo@gmail.com>
2022-08-10 10:37:27 -0400

    Update Skeleton components so that it respect the colorMode setting

---
+++ src/ui/Footer.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { Flex, Icon } from '@chakra-ui/react';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
+import { FOOTER_HEIGHT } from '../constants';
+import PageButton from './PageButton';
+import { Navigator, ReaderState } from '../types';
+import useColorModeValue from './hooks/useColorModeValue';
+
+type FooterProps = {
+  state: ReaderState | null;
+  navigator: Navigator | null;
+} & React.ComponentProps<typeof Flex>;
+
+const Footer: React.FC<FooterProps> = ({
+  state,
+  navigator,
+  ...rest
+}): JSX.Element => {
+  const bgColor = useColorModeValue('ui.white', 'ui.black', 'ui.sepia');
+  const isAtStart = state?.atStart;
+  const isAtEnd = state?.atEnd;
+  return (
+    <Flex
+      as="footer"
+      position="sticky"
+      height={`${FOOTER_HEIGHT}px`}
+      zIndex="sticky"
+      bottom="0"
+      justifyContent="space-between"
+      w="100%"
+      bg={bgColor}
+      borderTop="1px solid"
+      borderColor="gray.100"
+      {...rest}
+    >
+      <PageButton
+        onClick={navigator?.goBackward}
+        aria-label="Previous Page"
+        disabled={isAtStart}
+      >
+        <Icon as={MdKeyboardArrowLeft} w={6} h={6} />
+        Previous
+      </PageButton>
+      <PageButton
+        onClick={navigator?.goForward}
+        aria-label="Next Page"
+        disabled={isAtEnd}
+      >
+        Next <Icon as={MdKeyboardArrowRight} w={6} h={6} />
+      </PageButton>
+    </Flex>
+  );
+};
+
+export default Footer;
