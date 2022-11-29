129600c3d5a43d9f9151d09660458783341cf0b4
Olivia Wong <oliviawong@nypl.org>
2022-11-29 18:40:29 -0500

    Cypress tweaks

--- cypress/integration/multi-pdf/render-app.ts
+++ cypress/integration/multi-pdf/render-app.ts
@@ -6,7 +6,7 @@ describe('Renders multi PDF', () => {
     cy.findByRole('link', { name: 'Return to Homepage' }).should('exist');
     cy.findByRole('button', { name: 'Table of Contents' }).should('exist');
     cy.findByRole('button', { name: 'Settings' }).should('exist');
-    cy.findByRole('button', { name: 'Full screen' }).should('exist');
+    cy.findByRole('button', { name: 'Toggle full screen' }).should('exist');
     cy.findByRole('button', { name: 'Next Page' }).should('not.be.disabled');
     cy.findByRole('button', { name: 'Previous Page' }).should('be.disabled');
 
