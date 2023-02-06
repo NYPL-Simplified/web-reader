d95b8c2550e2de5795f597cfc8d0945c52f236ad
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2023-02-06 23:10:45 +0100

    use map instead of let variable

--- src/PdfReader/addTocToManifest.tsx
+++ src/PdfReader/addTocToManifest.tsx
@@ -1,39 +1,6 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { pdfjs } from 'react-pdf';
 import { ReadiumLink } from '../WebpubManifestTypes/ReadiumLink';
 import { WebpubManifest } from '../types';
-
-const addTocItem = async (
-  pdf: any,
-  outline: any[],
-  pdfUrl: string,
-  tocItems: ReadiumLink[]
-): Promise<ReadiumLink[]> => {
-  // iterate through each chapter
-  for (let i = 0; i < outline.length; i++) {
-    const chapter = outline[i];
-    const dest = chapter.dest;
-    if (dest && dest.length > 0) {
-      // get the page referance
-      const ref = dest[0];
-      const pageIndex = await pdf.getPageIndex(ref);
-      // TODO: Uncomment subchapter handling when goToPage is updated
-      // let subChapters: ReadiumLink[] = [];
-      // const outlineChildren = chapter.items;
-      // if (outlineChildren.length > 0) {
-      //   subChapters = await addTocItem(pdf, outlineChildren, pdfUrl, []);
-      // }
-      if (pageIndex) {
-        tocItems.push({
-          title: chapter.title,
-          href: `${pdfUrl}#page=${pageIndex + 1}`,
-          children: [], // TODO: replace with subChapters when goToPage is updated
-        });
-      }
-    }
-  }
-  return Promise.all(tocItems);
-};
 
 /**
  * Adds TOC data to Webpub Manifest from single-resource PDF using PDF.js
@@ -49,10 +16,27 @@ export default async function addTocToManifest(
   try {
     const pdf = await pdfjs.getDocument(proxiedUrl).promise;
     const outline = await pdf.getOutline(); // get the TOC outline
-    let toc: ReadiumLink[] = [];
-    if (outline) {
-      toc = await addTocItem(pdf, outline, pdfUrl, []);
-    }
+    const tocPromises = outline.map(async (chapter) => {
+      const dest = chapter.dest;
+      if (dest && dest.length > 0) {
+        // get the page referance
+        const ref = dest[0];
+        const pageIndex = await pdf.getPageIndex(ref);
+        if (pageIndex) {
+          const link: ReadiumLink = {
+            title: chapter.title,
+            href: `${pdfUrl}#page=${pageIndex.num + 1}`,
+            children: [],
+          };
+          return link;
+        }
+      }
+      return undefined;
+    });
+    // await all the promises and filter any undefined values (tere was no chapter.dest or pageIndex)
+    const toc = (await Promise.all(tocPromises)).filter(
+      (item): item is ReadiumLink => !!item
+    );
     if (toc.length > 0) {
       manifest.toc = toc;
     }
