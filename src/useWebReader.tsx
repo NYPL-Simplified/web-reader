fda0fa4a16f70da7e6882ec6a2833c1d0bac2ad6
Jackie Quach <jackiequach14@gmail.com>
2024-06-14 11:14:35 -0400

    add param to usePdfReader

--- src/useWebReader.tsx
+++ src/useWebReader.tsx
@@ -94,6 +94,7 @@ export default function useWebReader(
       ? {
           webpubManifestUrl,
           manifest,
+          getContent,
           proxyUrl,
           pdfWorkerSrc,
           height,
