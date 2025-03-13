b064a3a2e5de61dbb463f147d3d73541b503354e
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-03-13 15:21:36 -0600

    Ignored playwright folder in jest config

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,7 @@ const config: Config.InitialOptions = {
     '.(js|jsx)$': require.resolve('babel-jest'), // jest's default
   },
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
+  testPathIgnorePatterns: ['/playwright/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}'],
   testMatch: ['<rootDir>/**/*.(spec|test).{ts,tsx,js,jsx}'],
