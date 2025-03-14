d6fcdf6580611538d382a7854bf4cddcda03de94
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-03-14 09:45:38 -0600

    Addressed feedback

--- playwright/tests/test.spec.ts
+++ playwright/tests/test.spec.ts
@@ -1,9 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('Open moby-epub3', async ({ page }) => {
-  await page.goto('http://localhost:1234/html/moby-epub3');
-  //await expect(page.getByRole('link', { name: 'Back' })).toBeVisible(); // does not work
-  //await expect(page.getByRole('paragraph', { name: 'Back' })).toBeVisible(); // does not work
-  //await expect(page.getByText('Back')).toBeVisible(); // works but could be found in epub
-  await expect(page.getByLabel('Return to Homepage')).toBeVisible(); // works
+  await page.goto('/html/moby-epub3');
+  await expect(page.getByLabel('Return to Homepage')).toBeVisible();
 });
